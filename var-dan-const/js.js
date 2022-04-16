cb6acceea3a0d2f74520e0cf861cdb466c9b6e44
Ihat.fox@gmail.com <Ihat.fox@gmail.com>
2022-04-16 11:57:33 +0800

    var dan const

---
+++ var-dan-const/js.js
@@ -0,0 +1,15 @@
+// let sama sperti var tapi dia itu bis error
+console.log(i);
+var i = 5;
+// tapi kalo let dia error sama kaya bahsa pemograman lainnya
+// console.log(o)
+// let o = 5;
+//kalo const itu ketika lu buat sebuah varibleyang mugkin lu ngga bakal ganti nilai nya karna apabila lu ganti maka akan error
+// let
+let  f = 10;
+f = 5;
+console.log(f);
+// const
+const c = 10;
+c = 5;
+console.log(c);
