983ec091d66e754c85248adfadba53364b3eaca8
Ihat.fox@gmail.com <Ihat.fox@gmail.com>
2022-05-26 11:32:46 +0800

    26-05-2022-11:32 looping baru

---
+++ loopingBaru/script.js
@@ -0,0 +1,72 @@
+// looping baru
+// for.. of
+// for..in
+
+//for..of
+//"creates a loop lterating over interble object"
+// lterable object
+// string
+// array
+// arguments/nodelist
+// typedArray
+// map
+// set
+// user-difined ltrables
+
+// array 
+// const data = ['ihat','uhat','sapi']
+// for(const m of data){
+//     console.log(m);
+// }
+// //string
+// const nama = 'ihat'
+// for(const i of nama){
+//     console.log(i)
+// }
+// // jadi i medefnisikan isi dari string  
+
+// const data = ['ihat']
+// for(const [i,nama] of data.entries()){
+//     console.log(`hai nama saya ${nama} kaya rangking ${i}`)
+// }
+// kalo data.entries() dai jadi ada dua i itu index nama itu mendefinisiskan setiap array
+
+// node list
+// const liNama = document.querySelectorAll('.nama')
+// for(n of liNama){
+//     console.log(n.innerHTML)
+// }
+
+// // arguments
+// function jumlah(){
+//     let jumlah = 0;
+//     for(a of arguments){
+//         jumlah += a
+//     }
+//     return jumlah;
+// }
+
+// console.log(jumlah(1,2,3,4,5))
+
+// for..in
+// "creates a loop olny ltreting over enumrable"
+
+// const data = {
+//     nama: 'ihat',
+//     umur: 89,
+//     id: 5636
+// }
+
+// for(m in data){
+//     console.log(data[m]);
+// }
+// for (m in data) {
+//     console.log(m);
+// }
+
+
+
+
+
+
+
