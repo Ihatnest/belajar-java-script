2fc53ab2ff4140b7e550ff37cfea58c4bcc8d4dd
Ihatnest <85156361+Ihatnest@users.noreply.github.com>
2022-07-06 17:04:01 +0700

    t

---
+++ class/constructorFunction/membuatconstructorFunction/script.js
@@ -0,0 +1,12 @@
+function Nama() {
+  this 
+}
+const naga = new Nama()
+
+try {
+  
+} catch (error) {
+  
+}
+
+test('should first', () => { second })
---
+++ class/script.js
@@ -0,0 +1,12 @@
+class nama {
+  constructor(nama,kelas){
+    this.nama = nama
+    this.kelas = kelas
+  }
+  umur(naga){
+    console.log(`hai ${this.nama} ${naga}`)
+  }
+}
+
+const sapi = new nama('sapi',20)
+sapi.umur("naga")
